e9ce202f71246c1d47d282e0ab9ea8ca4f98a558
Kyle Twogood <ktwogood@hawaii.edu>
2016-03-31 14:12:30 -1000

    since game data model is only a single class, added to level-design branch pending additional data, once available

---
+++ Wai`Opae_project/Assets/models/fbx/GameModel.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameModel : MonoBehaviour
+{
+
+	public event EventHandler GameSuspendedChanged;
+	public event EventHandler LevelChanged;
+
+	public const int MinLevel = 1;
+	public const int MaxLevel = Int32.MaxValue;
+
+	private int level = 1;
+	public int Level
+	{
+		get
+		{
+			return level;
+		}
+
+		set
+		{
+			if (value != level)
+			{
+				if (value < MinLevel || value > MaxLevel)
+				{
+					Debug.Log("refused out of range level set: " + value);
+				}
+				else
+				{
+					level = value;
+					LevelChanged.Invoke(this, new EventArgs());
+				}
+			}
+		}
+	}
+
+	protected bool gameSuspended = true;
+	public bool GameSuspended
+	{
+		get
+		{
+			return gameSuspended;
+		}
+
+		set
+		{
+			if (gameSuspended != value)
+			{
+				gameSuspended = value;
+				GameSuspendedChanged.Invoke(this, new EventArgs());
+			}
+		}
+	}
+
+	private static readonly GameModel MODEL_SINGLETON = new GameModel();
+
+	public static GameModel getInstance()
+	{
+		return MODEL_SINGLETON;
+	}
+
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+}
