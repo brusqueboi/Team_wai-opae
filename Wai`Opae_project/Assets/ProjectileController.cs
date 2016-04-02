cf3987db6beada0982d1c058a3ddd088fa651b51
Kyle Twogood <ktwogood@hawaii.edu>
2016-04-01 15:24:34 -1000

    added projectile launcher script. To use, attach script to projectile GameObject and drag Target GameObject from heirarchy view into "Target" field in Inspector.

---
+++ Wai`Opae_project/Assets/ProjectileController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour
+{
+	public Transform Target;
+	public float firingAngle = 45.0f;
+	public float gravity = 9.8f;
+
+	protected Transform Projectile;
+	private Vector3 originalPos; // Debug. 
+
+	void Awake()
+	{
+		Projectile = gameObject.transform;
+		originalPos = gameObject.transform.position;
+	}
+
+	void Start()
+	{
+		FireProjectile();
+	}
+
+	void FireProjectile()
+	{
+		if (Target != null)
+		{
+			StartCoroutine(SimulateProjectile());
+		}
+		else
+		{
+			Debug.Log("FireProjectile() failed: unspecified target");
+		}
+	}
+
+
+	IEnumerator SimulateProjectile()
+	{
+		while(true)
+		{
+			//Sources:
+			//http://en.wikipedia.org/wiki/Trajectory_of_a_projectile
+			//http://forum.unity3d.com/threads/throw-an-object-along-a-parabola.158855/#post-1087673
+
+			// Calculate distance to target
+			float target_Distance = Vector3.Distance(Projectile.position, Target.position);
+
+			// Calculate the velocity needed to throw the object to the target at specified angle.
+			float projectile_Velocity = target_Distance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / gravity);
+
+			// Extract the X  Y componenent of the velocity
+			float Vx = Mathf.Sqrt(projectile_Velocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
+			float Vy = Mathf.Sqrt(projectile_Velocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
+
+			// Calculate flight time.
+			float flightDuration = target_Distance / Vx;
+
+			// Rotate projectile to face the target.
+			Projectile.rotation = Quaternion.LookRotation(Target.position - Projectile.position);
+
+			float elapse_time = 0;
+
+			while (elapse_time < flightDuration)
+			{
+				Projectile.Translate(0, (Vy - (gravity * elapse_time)) * Time.deltaTime, Vx * Time.deltaTime);
+
+				elapse_time += Time.deltaTime;
+
+				yield return null;
+			}
+
+			// Reset and restart for demo.
+			yield return new WaitForSeconds(2.0f);
+			Projectile.position = originalPos;
+		}
+
+	}
+}
